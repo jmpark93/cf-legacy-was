a0df1f993671994bfabfedc8764d8ef42130475d
Jongmin Park <jmpark93@koscom.co.kr>
2020-05-27 18:07:07 +0900

    Initial commit ...

---
+++ src/main/java/com/jmworks/todoapi/TodoapiApplication.java
@@ -0,0 +1,19 @@
+package com.jmworks.todoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+
+@SpringBootApplication
+@EnableSwagger2WebMvc
+@Import(SpringDataRestConfiguration.class)
+public class TodoapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoapiApplication.class, args);
+    }
+
+}
