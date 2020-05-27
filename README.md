052db38184c7d508fbc163096af57959eaef3f27
Jongmin Park <jmpark93@koscom.co.kr>
2020-05-27 18:37:40 +0900

    update ...

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # cf-legacy-was
+
+### Build & Run Application
+
+* run Application 
+```
+$ ./gradlew bootRun
+```
+
+* build jar & run Application 
+```
+$ ./gradlew build
+$ java -jar build/libs/todoapi-0.0.1-SNAPSHOT.jar
+```
+
+### Usage
+
+* Swagger URL 
+```
+http://localhost:8080/swagger-ui.html
+```
+
+### Links
+
+* [JPA Data with REST](https://spring.io/guides/gs/accessing-data-rest/)
+* [Swagger 2 with a Spring REST API](https://www.baeldung.com/swagger-2-documentation-for-spring-rest-api)
--- src/main/java/com/jmworks/todoapi/TodoapiApplication.java
+++ src/main/java/com/jmworks/todoapi/TodoapiApplication.java
@@ -2,14 +2,8 @@ package com.jmworks.todoapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
-import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 @SpringBootApplication
-@EnableSwagger2WebMvc
-@Import(SpringDataRestConfiguration.class)
 public class TodoapiApplication {
 
     public static void main(String[] args) {
